71c683da9dd3d2912fa039624c3441324916bf8d
vraj-s <vraj.shah.028@gmail.com>
2025-11-25 00:56:45 +1100

    Removed unnecessary variables

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ headers = {
     'Accept' : 'application/json'
 }
 
-data = 'grant_type=client_credentials&client_id=59a133469ffc487c824e4d97122954db&client_secret=579debde9a064dc3a586e213d58f23c8'
 
 url = 'https://api.reccobeats.com/v1/track/2a716c67-b388-4250-8abb-a127d7c78856/audio-features'
 track_link = ''
