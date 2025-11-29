ba9c015b834044b4aa54bed2cd7e3897e3325057
vraj-s <vraj.shah.028@gmail.com>
2025-11-29 14:54:45 +1100

    Included list of parameters for clustering

---
+++ clustering/read_csv.py
@@ -0,0 +1,10 @@
+import pandas as pd
+path = r'clustering\data\tracks_features.csv'
+# the info we want to keep in the dataframe for 
+track_details = ["id", "name" , "artists"]
+parameters = ["acousticness", "danceability", "energy", "instrumentalness", "loudness", "speechiness", "tempo", "valence"]
+
+df = pd.read_csv(path)
+
+print(df.head())
+
